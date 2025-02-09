a43f05fcb33faf61f820d6e60b07c4af1a9a6b82
KYamaguchi-nagaoka <nzols91015@yahoo.co.jp>
2025-02-09 13:01:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if uploaded_image is not None:
 
     # 予測の実行
     if st.button("Predict", key="predict_button"):
-        response = requests.post("http://localhost:8000/predict", files={'file': uploaded_image.getvalue()})
+        response = requests.post("https://asari-classification.onrender.com", files={'file': uploaded_image.getvalue()})
         result=response.json()
         prediction = result["prediction"]
         probability = result["probability"]
